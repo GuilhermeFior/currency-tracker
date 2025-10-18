c0efcd190f463e9c886840f17823eebbda480981
Guilherme Fior <guilhermefiorribeiro@gmail.com>
2025-10-18 16:29:03 -0300

    Removing some env config from dag

--- dags/fx_pipeline_dag.py
+++ dags/fx_pipeline_dag.py
@@ -4,9 +4,6 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
-# Se você usar Cloud Run, troque por um operador customizado/Hook.
-# Aqui vamos ilustrar rodando o scraper local via Docker (ambiente do worker precisa ter Docker).
-
 DEFAULT_ARGS = {
     "owner": "GuilhermeFior",
     "depends_on_past": False,
@@ -25,55 +22,43 @@ with DAG(
     tags=["fx", "dbt", "bigquery"],
 ) as dag:
 
-    docker_scraper = BashOperator(
+    extract_to_raw = BashOperator(
         task_id="extract_to_raw",
-        bash_command="python /project/services/scraper/app.py",
-        env={
-            "BQ_PROJECT_ID": "{{ var.value.BQ_PROJECT_ID }}",
-            "BQ_DATASET_RAW": os.getenv("BQ_DATASET_RAW", "fx_raw"),
-            "BQ_TABLE_RAW": os.getenv("BQ_TABLE_RAW", "prices_raw"),
-            "BQ_LOCATION": os.getenv("BQ_LOCATION", "US"),
-            "BASE_CURRENCY": os.getenv("BASE_CURRENCY", "BRL"),
-            "GOOGLE_APPLICATION_CREDENTIALS": "/keys/sandbox-personal-projects-26ad98d02f4f.json",
-        },
+        bash_command="python /project/services/scraper/app.py"
     )
 
     # Rodando dbt via Bash (o ambiente do worker precisa ter dbt instalado/configurado)
     dbt_deps = BashOperator(
         task_id="dbt_deps",
-        bash_command="cd /project/dags/../dbt && dbt deps",
+        bash_command="cd /project/dags/../dbt && dbt deps --profiles-dir ~/.dbt"
     )
 
     dbt_staging = BashOperator(
         task_id="dbt_staging",
         bash_command=(
-            "cd /project/dags/../dbt && "
-            "dbt run --select staging --profiles-dir ~/.dbt"
+            "cd /project/dags/../dbt && dbt run --select staging --profiles-dir ~/.dbt"
         ),
     )
 
     dbt_tests = BashOperator(
         task_id="dbt_tests",
         bash_command=(
-            "cd /project/dags/../dbt && "
-            "dbt test --select staging+ --profiles-dir ~/.dbt"
+            "cd /project/dags/../dbt && dbt test --select staging+ --profiles-dir ~/.dbt"
         ),
     )
 
     dbt_silver = BashOperator(
         task_id="dbt_silver",
         bash_command=(
-            "cd /project/dags/../dbt && "
-            "dbt run --select silver --profiles-dir ~/.dbt"
+            "cd /project/dags/../dbt && dbt run --select silver --profiles-dir ~/.dbt"
         ),
     )
 
     dbt_gold = BashOperator(
         task_id="dbt_gold",
         bash_command=(
-            "cd /project/dags/../dbt && "
-            "dbt run --select gold --profiles-dir ~/.dbt"
+            "cd /project/dags/../dbt && dbt run --select gold --profiles-dir ~/.dbt"
         ),
     )
 
-    docker_scraper >> dbt_deps >> dbt_staging >> dbt_tests >> dbt_silver >> dbt_gold
+    extract_to_raw >> dbt_deps >> dbt_staging >> dbt_tests >> dbt_silver >> dbt_gold
